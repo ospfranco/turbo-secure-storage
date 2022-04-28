82c7b977d07f4132bb87b8291df3dad50f9093de
Oscar Franco <ospfranco@protonmail.com>
2022-04-28 11:55:11 +0200

    Clean up App code

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Alert, Button, SafeAreaView } from 'react-native';
-import TurboSecureStorage from 'turbo-secure-storage';
+import TurboSecureStorage, { ACCESSIBILITY } from 'turbo-secure-storage';
 
 const App = () => {
   return (
@@ -9,10 +9,9 @@ const App = () => {
         title="Set Item"
         onPress={() => {
           const { error } = TurboSecureStorage.setItem('foo', 'bar', {
-            // accessibility: ACCESSIBILITY.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY,
+            accessibility: ACCESSIBILITY.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY,
             biometricAuthentication: true,
           });
-          // const { error } = TurboSecureStorage.setItem('foo', 'bar');
           if (error) {
             Alert.alert('Could not save string');
           }
